23f32d6b8f7586b50e75731f2f528fdc45080ae2
Myron Franze <58065110+Norym@users.noreply.github.com>
2020-06-17 11:18:10 +0200

    v1.0.0 (#2)
    * added siguni v1.0.0

---
+++ driver/gtest/hal_test.cpp
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPLv3-or-later */
+/* Copyright (c) 2020 Project WomoLIN */
+/* Author Myron Franze <myronfranze@web.de> */
+
+#include <gtest/gtest.h>
+
+#include "../hal.h"
+
+namespace pcsim::driver::gtest
+{
+
+class CHalTest  : public ::testing::Test { };
+
+
+TEST_F( CHalTest, TODO ) {
+   auto test = pcsim::driver::CHal();
+}
+
+}
